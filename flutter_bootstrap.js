b9b2f1c6d5c02a226fbda01fa6d1d24ffce33361
codeuga <anugrahdwi1005@gmail.com>
2024-08-11 22:42:53 +0700

    add link project

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2770046103"
+    serviceWorkerVersion: "3045446481"
   }
 });
